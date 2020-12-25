fb27f5a8a10e4a8badaf6cd8e63e9c7151b69ea2
ksound22 <chrisjay967@gmail.com>
2020-12-26 00:26:53 +0100

    made changes to portfolio project

--- portfolioProject/starter/script.js
+++ portfolioProject/starter/script.js
@@ -35,13 +35,13 @@ function setTheme(mode) {
 
 // Name animation
 let text = document.querySelector("#anim");
-console.log(text);
+// console.log(text);
 
 let strText = text.textContent;
-console.log(strText);
+// console.log(strText);
 
 const splitText = strText.split("");
-console.log(splitText);
+// console.log(splitText);
 
 text.textContent = "";
 for (i = 0; i < splitText.length; i++) {
@@ -64,4 +64,69 @@ function onTick() {
 function complete() {
   clearInterval(timer);
   timer = null;
+}
+
+// Scroll to top scritpt
+const ScrollToTop = document.querySelector("#ScrollToTop");
+
+ScrollToTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+// Form Validation
+const form = document.querySelector(".form");
+const cname = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("working");
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // Getting the values of the inputs
+  const nameValue = cname.value.trim();
+  const emailValue = email.value.trim();
+  const messageValue = message.value.trim();
+
+  if (nameValue === "") {
+    setErrorFor(cname, "Name cannot be blank");
+  } else {
+    setSuccessFor(cname);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "email must be valid");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (messageValue.length <= 139) {
+    setErrorFor(message, "Message must be up to 140 characters");
+  } else {
+    setSuccessFor(message);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
